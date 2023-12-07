d0d6d9c3199a062303510359b6537b7741ac6c98
Tes Ryu <505032@waes.ac.uk>
2023-12-07 11:25:03 +0000

    Update multiply

--- assets/javascript/multiply-loop.js
+++
@@ -1,8 +0,0 @@
-/* Code to demonstrate the FOR loop */
-
-let firstNumber = promt("Please enter first number: ");
-let secondNumber = promt("Please enter second number: ");
-
-for ( counter = 1 ; counter <= 10 ; counter = counter + 1 ){
-    document.write("<br>", firstNumber, " X ", secondNumber, " = ", firstNumber * secondNumber);
-}
---
+++ assets/javascript/multiply.js
@@ -0,0 +1,6 @@
+
+let firstNumber = promt("Please enter first number: ");
+let secondNumber = promt("Please enter second number: ");
+
+document.write("<p>", firstNumber * secondNumber, "</p>");
+
