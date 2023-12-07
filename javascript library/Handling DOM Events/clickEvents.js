1f5d4ae757d1059880417ad750fc6b9cbbe6dd9f
Tes Ryu <505032@waes.ac.uk>
2023-12-07 16:25:22 +0000

    Add clickEvents

---
+++ javascript library/Handling DOM Events/clickEvents.js
@@ -0,0 +1,11 @@
+
+ /* Prompt the user to enter a name */
+ newName= prompt("Please enter a new student name: ");
+
+ /* Ask the user for new names, stop if the name is blank */
+ 
+ while (newName != "") {
+     document.write("<p>", newName, "</p>");
+ }else{
+ document.write("<p> No more students. </p>");
+ }
