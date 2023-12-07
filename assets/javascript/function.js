7cf95c3b8c8adb5905fc083f9f960fcf41fc033f
Tes Ryu <505032@waes.ac.uk>
2023-12-07 15:29:08 +0000

    Update function

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -1,9 +1,15 @@
 let students = ["Pebels", "BamBam", "Fred", "Will"];
 
-function printArray(){
+function printArray(myHeading){
+    document.write("<h1>", myHeading, "</h1>");
+    document.write("<hr>");
     for (let i = 0; i < students.length ; i = i +1 ){
         document.write("<p>", students[i], "</p>");
     };
 }
+
+printArray("Oringinal Array");
+students.push("Tes");
+printArray("Updated Array");
 
 printArray();
