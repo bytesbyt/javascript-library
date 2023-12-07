c7ff5db22cf7224c3d24d1d3846a9f786289ed6c
Tes Ryu <505032@waes.ac.uk>
2023-12-07 11:09:11 +0000

    Add loop through the array

--- assets/javascript/array.js
+++ assets/javascript/array.js
@@ -1,7 +1,13 @@
+//Define array
 let students = ["Pebbles", "Bam-Bam", "Wednesday", "Tes", "Beth"]
-
+//Index
 document.write("<p>", students[0], "</p>");
 document.write("<p>", students[3], "</p>");
-
+//How manny students are there?
 let studentsCount = students.length;
 document.write("<p> Number of students= ", studentsCount, "</p>");
+
+//Loop through the array, printing the student name
+for (let i = 0; i < students.length; i= i + 1 ){
+    document.write("<p>", i, " : ", students[i], "</p>");
+}
