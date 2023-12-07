0737f35939f874ae7e846323e88aacfb06153dc1
Tes Ryu <505032@waes.ac.uk>
2023-12-07 10:24:46 +0000

    fix innerHTML bug

--- assets/javascript/multiply-loop.js
+++ assets/javascript/multiply-loop.js
@@ -1,8 +1,7 @@
 /* Code to demonstrate the FOR loop */
 
-let firstNumber = promt("Please enter first number: ")
-
-let secondNumber = promt("Please enter second number: ")
+let firstNumber = promt("Please enter first number: ");
+let secondNumber = promt("Please enter second number: ");
 
 for ( counter = 1 ; counter <= 10 ; counter = counter + 1 ){
     document.write("<br>", firstNumber, " X ", secondNumber, " = ", firstNumber * secondNumber);
