90575fd40489b48fca474737aa56620e0846e89a
Tes Ryu <505032@waes.ac.uk>
2023-12-07 10:57:14 +0000

    Update array file

--- assets/javascript/array.js
+++ assets/javascript/array.js
@@ -1,4 +1,7 @@
-let students = ["Pebbles", "Bam-Bam", "Wednesday", "Tes"]
+let students = ["Pebbles", "Bam-Bam", "Wednesday", "Tes", "Beth"]
 
 document.write("<p>", students[0], "</p>");
-document .write("<p>", students[3], "</p>");
+document.write("<p>", students[3], "</p>");
+
+let studentsCount = students.length;
+document.write("<p> Number of students= ", studentsCount, "</p>");
