1ba924dc8b1cbbcda099e863b37747cb0b9c17f2
Tes Ryu <505032@waes.ac.uk>
2023-12-08 16:44:40 +0000

    Add clickEvents to javascript library.

--- javascript-library/DOM/clickEvents.js
+++ javascript-library/DOM/clickEvents.js
@@ -1,3 +1,20 @@
-<button onclick="alert('Hello! You clicked the button!');">
-  Click to Trigger Alert!
-</button>
+function changeBox() {
+    console.log("Changes incoming!");
+
+    // Get the div
+    let lowlyDiv = document.getElementById('lowly-div');
+
+    /* We can check the box to determine its width and
+    change colors accordingly. 120 = 100px + 10px padding
+    on each side. Check style.css */
+
+    if (lowlyDiv.clientWidth === 120) {
+        // Change its background color and width
+        lowlyDiv.style.backgroundColor = "lightgreen";
+        lowlyDiv.style.width = '200px';
+    } else {
+        // Change it back to normal
+        lowlyDiv.style.backgroundColor = "lightyellow";
+        lowlyDiv.style.width = '100px';
+    }
+}
