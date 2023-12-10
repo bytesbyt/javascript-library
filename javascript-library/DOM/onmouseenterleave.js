39837eb3deb147a6fd010316c895a763c0d26bda
Tes <101018180+bytesbyt@users.noreply.github.com>
2023-12-10 16:33:24 +0000

    Add onmouseenter/onmouseleave

---
+++ javascript-library/DOM/onmouseenterleave.js
@@ -0,0 +1,9 @@
+function turnPink(){
+    let box = document.getElementById("box");
+    box.style.backgroundColor = "lightpink";
+}
+
+function turnYellow(){
+    let box = document.getElementById("box");
+    box.style.backgroundColor = "yellow";
+}
