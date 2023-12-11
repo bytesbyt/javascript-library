adeeb5c8020444de82f995009b773644f96caaed
Tes <101018180+bytesbyt@users.noreply.github.com>
2023-12-11 21:10:03 +0000

    Add prepopulating form

---
+++ javascript-library/DOM/form/prepopulatingForm.js
@@ -0,0 +1,8 @@
+let fname = document.getElementById('first-name');
+fname.value = "Indiana";
+let lname = document.getElementById('last-name');
+lname.value = "Jones";
+let headline = document.getElementById('headline');
+headline.value = "If you want to be a good Archaeologist, you gotta get out of the library.";
+let position = document.getElementById('position');
+position.value = "Archaeologist at Marshall College";
